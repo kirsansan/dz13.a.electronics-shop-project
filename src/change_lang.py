5aae53a92c703a017ca313214171fd6d10c59f65
kirsansan <kirill_home@mail.ru>
2023-05-01 22:00:54 +0300

    add KeyBoard class, add ChangeLangMixin class

---
+++ src/change_lang.py
@@ -0,0 +1,20 @@
+from constant.constants import LANG_LIST
+
+
+class ChangeLangMixin:
+
+    def __init__(self, name: str, price: float, quantity: int):
+        super().__init__(name, price, quantity)
+        self.__lang = "EN"
+
+    def change_lang(self):
+        """Change the language from LANG_LIST to anything non-equal to the current language"""
+        for challenger in LANG_LIST:
+            if self.__lang != challenger:
+                self.__lang = challenger
+                break
+        return self
+
+    @property
+    def language(self):
+        return self.__lang
---
+++ src/keyboard.py
@@ -0,0 +1,15 @@
+from src.item import Item
+from src.change_lang import ChangeLangMixin
+
+
+class KeyBoard(ChangeLangMixin, Item):
+    pass
+
+
+    # @property
+    # def language(self):
+    #     return self.__lang
+
+    # @language.setter
+    # def language(self):
+    #     self.__language =
